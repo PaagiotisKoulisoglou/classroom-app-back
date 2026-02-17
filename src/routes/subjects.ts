56d4f1189b82cfc85722c36b7f04b2cd4120fff8
PanagiotisKoulisoglou <panagiotiskoulisoglou@gmail.com>
2026-02-18 01:17:58 +0200

    Implement suggestions

--- src/routes/subjects.ts
+++ src/routes/subjects.ts
@@ -29,6 +29,8 @@ router.get('/', async (_req , res) => {
 
         if (department) {
             filterConditions.push(ilike(departments.name, `%${department}%`));
+            const deptPattern = `%${String(department).replace(/[%_]/g, '\\$&')}%`;
+            filterConditions.push(ilike(departments.name, deptPattern));
         }
 
         const whereClause = filterConditions.length > 0 ? and(...filterConditions) : undefined as any;
